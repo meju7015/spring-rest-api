319e0edfa58cacf58fd8bd1e11ff4cd00ac46274
정주호 <meju7015@gmail.com>
2021-02-18 11:14:45 +0900

    feat ADD Exceptions

---
+++ src/main/java/com/stick/rest/advice/exception/CUserExistException.java
@@ -0,0 +1,16 @@
+package com.stick.rest.advice.exception;
+
+public class CUserExistException extends RuntimeException {
+
+    public CUserExistException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public CUserExistException(String msg) {
+        super(msg);
+    }
+
+    public CUserExistException() {
+        super();
+    }
+}
