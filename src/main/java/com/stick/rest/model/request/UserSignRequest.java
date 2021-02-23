540d67dfe36c308fa67ad23afa6e0c1fb46e51d7
정주호 <meju7015@gmail.com>
2021-02-23 13:41:37 +0900

    Add 로그인 리퀘스트 생성
    .

---
+++ src/main/java/com/stick/rest/model/request/UserSignRequest.java
@@ -0,0 +1,13 @@
+package com.stick.rest.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSignRequest {
+
+    private String email;
+    private String password;
+
+}
